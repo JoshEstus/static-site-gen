0bf562397e674d16b8c3f790424817aa0165a865
JoshEstus <josh.estus@gmail.com>
2025-04-25 10:16:29 -0400

    Add copy tree for static to public and add the static content

--- src/main.py
+++ src/main.py
@@ -1,8 +1,25 @@
+import shutil
+import os
+
 from textnode import TextNode, TextType
 
 def main():
-    text = TextNode("Some text", TextType.Link, "https://www.google.com")
-    print(text)
+    copy_static_to_public()
+
+
+
+def copy_static_to_public():
+    public_path = "./public/"
+    static_path = "./static/"
+    if os.path.exists(public_path):
+        print("Removing public...")
+        shutil.rmtree("./public/")
+    else:
+        print("public folder does not exist, making dir...")
+        os.mkdir(public_path)
+
+    print("Copying static to public...")
+    shutil.copytree(static_path, public_path)
 
 
 main()
